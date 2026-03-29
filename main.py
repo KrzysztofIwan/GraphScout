0a4013d19f5736f6e67275ecba558b6adc64388d
KrzysztofIwan <iwanski.krzysztof01@gmail.com>
2026-03-29 19:48:19 +0200

    dodanie pierwszego połączenia z chatem gemini

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from src.graph_logic import TrailGraph
+from src.gemini_client import GeminiClient
 
 def run_app():
     graf = TrailGraph()
     graf.build_trail_graph('data/trails/polish_tatra_mountains.json')
     #graf.save_visualization('data/visualizations')
-    graf.show_visualization()
+    #graf.show_visualization()
+
+    try:
+        gemini = GeminiClient()
+        gemini.generate_chat()
+        response = gemini.send_message("Powiedz mi jaką mamy godzinę i wylosuj losowy kolor.")
+        print(response.text)
+    except Exception as ex:
+        print(f"Wystąpił błąd:  {ex}")
 
 if __name__ == "__main__":
     run_app()
---
+++ src/gemini_client.py
@@ -0,0 +1,28 @@
+from google import genai
+import json
+import os
+import time
+
+class GeminiClient:
+    def __init__(self):
+        file_path = 'config/gemini_api.json'
+        config = self._load_config(file_path)
+        self.model = config.get('gemini_model').strip()
+        self.api_key = config.get('api_key').strip()
+        self.client = genai.Client(api_key = self.api_key)
+        self.chat = None
+        
+    def _load_config(self, json_path):
+        if not os.path.exists(json_path):
+            raise FileNotFoundError(f"Błąd: Nie znaleziono pliku {json_path}")
+        with open(json_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    def generate_chat(self):
+        self.chat = self.client.chats.create(model = self.model)
+
+    def send_message(self, message):
+        time.sleep(1) # Dodanie przestoju z uwagi na limitowaną liczbę zapytań
+        if not self.chat:
+            self.generate_chat() # Jeżeli chat nie istnieje tworzymy go na nowo
+        return self.chat.send_message(message)
